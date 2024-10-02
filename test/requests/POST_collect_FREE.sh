a4a7a15e272c7e4a4c328a4908b50ff10dc19bc5
LucBerge <lucas.bergeron@outlook.fr>
2024-10-02 14:37:11 +0200

    Fix arguments

--- test/requests/POST_collect_FREE.sh
+++ test/requests/POST_collect_FREE.sh
@@ -1,4 +1,4 @@
 curl -X POST \
     -H "Content-Type: application/json" \
-    -d "{\"webhook\":\"$0\",\"collector\":\"$1\",\"params\":{\"username\":\"$2\",\"password\":\"$3\"}}" \
+    -d "{\"webhook\":\"$1\",\"collector\":\"$2\",\"params\":{\"username\":\"$3\",\"password\":\"$4\"}}" \
     127.0.0.1:8080/api/v1/collect
