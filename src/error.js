61d5dfbe675a428ebe262b94a11d2027b934262a
LucBerge <lucas.bergeron@outlook.fr>
2024-10-30 17:21:50 +0100

    Add error name

--- src/error.js
+++ src/error.js
@@ -1,12 +1,14 @@
 class MissingField extends Error {
     constructor(field_name, opts) {
         super(`The field "${field_name}" is missing.`, opts);
+        this.name = this.constructor.name;
     }
 }
 
 class ElementNotFoundError extends Error {
     constructor(url, source_code, screenshot, selector, opts) {
         super(`Could not find selector '${selector.selector}' corresponding to the "${selector.info}" on the page '${url}'. See the source code and the screenshot to find the issue.`, opts);
+        this.name = this.constructor.name;
         this.url = url;
         this.source_code = source_code;
         this.screenshot = screenshot;
@@ -16,19 +18,22 @@ class ElementNotFoundError extends Error {
 
 class NotAuthenticatedError extends Error {
     constructor(opts) {
-        super(`Could not authenticate. Please verify your username and password.`, opts);
+        super(`Could not authenticate. Please verify your credentials.`, opts);
+        this.name = this.constructor.name;
     }
 }
 
 class InMaintenanceError extends Error {
     constructor(opts) {
         super(`The website is in maintenance. Wait a moment and try again.`, opts);
+        this.name = this.constructor.name;
     }
 }
 
 class UnfinishedCollector extends Error {
     constructor(url, source_code, screenshot, opts) {
         super(`The collector is not finished`, opts);
+        this.name = this.constructor.name;
         this.url = url;
         this.source_code = source_code;
         this.screenshot = screenshot;
