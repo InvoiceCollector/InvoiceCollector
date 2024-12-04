6a905ff0e26c0820e9f98e2f2fd979a74908f243
LucBerge <lucas.bergeron@outlook.fr>
2024-12-04 17:05:54 +0100

    Add `dotenv` package for dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const { StatusError } = require('./error.js')
 const { Server } = require('./server.js');
