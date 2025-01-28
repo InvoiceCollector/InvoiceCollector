1061a922ad43a436e75f7cdb1e4afc5944d39237
LucBerge <lucas.bergeron@outlook.fr>
2025-01-28 17:41:30 +0100

    Add optionnal parameter `download`, can download from `bytes` type

--- src/collectors/base_collector.ts
+++ src/collectors/base_collector.ts
@@ -10,7 +10,7 @@ export class AbstractCollector {
         this.config = config;
     }
 
-    async download(params, invoices) {
+    async download(params, invoices): Promise<void> {
         console.log(`Downloading ${invoices.length} invoices`);
 
         for(let invoice of invoices) {
@@ -21,6 +21,11 @@ export class AbstractCollector {
                 invoice.data = response.data.toString("base64");
                 invoice.type = "base64";
             }
+            else if(invoice.type == "bytes") {
+                invoice.data = btoa(String.fromCharCode.apply(null, invoice.bytes));
+                delete invoice.bytes;
+                invoice.type = "base64";
+            }
         }
 
         // Order invoices by timestamp
@@ -29,7 +34,7 @@ export class AbstractCollector {
 
     //NOT IMPLEMENTED
 
-    async collect(params) {
+    async collect(params, download=true): Promise<any[]> {
         throw new Error('`collect` is not implemented.');
     }
 }
@@ -49,11 +54,14 @@ export class ScrapperCollector extends AbstractCollector {
         height: 1080,
     };
 
+    authentication_error: string | null;
+
     constructor(config) {
         super(config);
+        this.authentication_error = null;
     }
 
-    async collect(params) {
+    async collect(params, download=true): Promise<any[]> {
         if(!params.username) {
             throw new Error('Field "username" is missing.');
         }
@@ -61,34 +69,34 @@ export class ScrapperCollector extends AbstractCollector {
             throw new Error('Field "password" is missing.');
         }
 
-        //Start browser
+        // Start browser
         let browser = await puppeteer.launch(this.PUPPETEER_CONFIG);
 
-        //Open new page
+        // Open new page
         let page = await browser.newPage();
         await page.setViewport(this.PAGE_CONFIG);
         await page.goto(this.config.entry_url);
 
         let driver = new Driver(page, this);
 
-        //Check if website is in maintenance
+        // Check if website is in maintenance
         const is_in_maintenance = await this.is_in_maintenance(driver, params)
         if (is_in_maintenance) {
             await browser.close()
             throw new InMaintenanceError(this.config.name, this.config.version);
         }
 
-        //Login
+        // Login
         await this.login(driver, params)
 
-        //Check if not authenticated
+        // Check if not authenticated
         const authentication_error = await this.get_authentication_error(driver, params)
         if (authentication_error) {
             await browser.close()
             throw new NotAuthenticatedError(authentication_error, this.config.name, this.config.version);
         }
 
-        //Collect invoices
+        // Collect invoices
         const invoices = await this.run(driver, params)
         if (invoices === undefined) {
             const url = await page.url();
@@ -99,8 +107,10 @@ export class ScrapperCollector extends AbstractCollector {
             throw new UnfinishedCollector(this.config.name, this.config.version, url, source_code_base64, screenshot);
         }
 
-        //Download invoices to token folder
-        await this.download(params, invoices);
+        // Download invoices to token folder
+        if(download) {
+            await this.download(params, invoices);
+        }
 
         // Close the borwser
         await browser.close()
@@ -110,20 +120,19 @@ export class ScrapperCollector extends AbstractCollector {
 
     //NOT IMPLEMENTED
 
-    async login(driver, params){
+    async login(driver, params): Promise<void>{
         throw new Error('`login` is not implemented.');
     }
 
-    async run(driver, params) {
+    async run(driver, params): Promise<any[]> {
         throw new Error('`run` is not implemented.');
     }
 
-    async get_authentication_error(driver, params){
-        //Assume the password is correct
-        return null;
+    async get_authentication_error(driver, params): Promise<string | null> {
+        return this.authentication_error;
     }
 
-    async is_in_maintenance(driver, params){
+    async is_in_maintenance(driver, params): Promise<boolean>{
         //Assume the website is not in maintenance
         return false;
     }
