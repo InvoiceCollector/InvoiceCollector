59b598ec7f2ea0f14a28a0dd10d8b70ff56a1527
LucBerge <lucas.bergeron@outlook.fr>
2025-02-08 18:12:56 +0100

    Add `date-fns` package

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import * as crypto from 'crypto';
+import date_fns from 'date-fns';
+import { enUS, fr } from 'date-fns/locale';
 
 export function generate_token(size=64): string {
     return crypto.randomBytes(size).toString('hex');
@@ -12,4 +14,15 @@ export function delay(ms) {
     return new Promise(function(resolve) { 
         setTimeout(resolve, ms)
     });
- }
+}
+
+export function timestampFromString(date: string, format: string, locale: string): number {
+    const fnsLocales = {
+        en: enUS,
+        fr: fr,
+    };
+
+    const dateFnsLocale = fnsLocales[locale];
+    const parsedDate = date_fns.parse(date, format, new Date(Date.UTC(1970, 0, 1)), { locale: dateFnsLocale });
+    return parsedDate.setUTCMilliseconds(0);
+}
