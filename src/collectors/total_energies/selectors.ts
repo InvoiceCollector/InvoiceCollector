826aafaf020a4b3b924219e228e824f12bd54bfd
LucBerge <lucas.bergeron@outlook.fr>
2025-02-09 18:05:46 +0100

    Add collector `total_energies` #15

---
+++ src/collectors/total_energies/selectors.ts
@@ -0,0 +1,31 @@
+export const TotalEnergiesSelectors = {
+    // COOKIES
+
+    BUTTON_REFUSE_COOKIES: {
+        selector: "#popin_tc_privacy_button",
+        info: "refuse cookies button"
+    },
+    BUTTON_ACCEPT_COOKIES: {
+        selector: "#popin_tc_privacy_button_3",
+        info: "accept cookies button"
+    },
+
+    // LOGIN PAGE
+
+    FIELD_EMAIL: {
+        selector: "#formz-authentification-form-login",
+        info: "email input"
+    },
+    FIELD_PASSWORD: {
+        selector: "#formz-authentification-form-password",
+        info: "password input"
+    },
+    BUTTON_SUBMIT: {
+        selector: "#js--btn-validation",
+        info: "submit button"
+    },
+    CONTAINER_LOGIN_ALERT: {
+        selector: ".text--alerte",
+        info: "login alert container"
+    },
+}
---
+++ src/collectors/total_energies/total_energies.ts
@@ -0,0 +1,56 @@
+import { ScrapperCollector } from '../scrapperCollector';
+import { TotalEnergiesSelectors } from './selectors';
+import { Driver } from '../../driver';
+import { Invoice } from '../abstractCollector';
+
+export class TotalEnergiesCollector extends ScrapperCollector {
+
+    static CONFIG = {
+        name: "Total Energies (Electricité & Gaz)",
+        description: "i18n.collectors.total_energies.description",
+        version: "1",
+        website: "https://www.totalenergies.fr",
+        logo: "https://upload.wikimedia.org/wikipedia/fr/f/f7/Logo_TotalEnergies.svg",
+        params: {
+            id: {
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true,
+            }
+        },
+        entryUrl: "https://www.totalenergies.fr/clients/connexion"
+    }
+
+    constructor() {
+        super(TotalEnergiesCollector.CONFIG);
+    }
+
+    async login(driver: Driver, params: any): Promise<string | void> {
+        // Refuse cookies
+        await driver.left_click(TotalEnergiesSelectors.BUTTON_REFUSE_COOKIES, false, 5000);
+
+        // Input email
+        await driver.input_text(TotalEnergiesSelectors.FIELD_EMAIL, params.id);
+        await driver.input_text(TotalEnergiesSelectors.FIELD_PASSWORD, params.password);
+        await driver.left_click(TotalEnergiesSelectors.BUTTON_SUBMIT);
+    
+        // Check if login alert
+        const login_alert = await driver.wait_for_element(TotalEnergiesSelectors.CONTAINER_LOGIN_ALERT, false, 5000);
+        if (login_alert) {
+            return await login_alert.evaluate(e => e.textContent) || "i18n.collectors.all.password.error";
+        }
+    }
+
+    async collect(driver: Driver, params: any): Promise<void> {
+        // TODO : Implement the rest of the collector
+    }
+
+    async download(driver: Driver, invoice: Invoice): Promise<void> {
+        // TODO : Implement the downloader
+    }
+}
